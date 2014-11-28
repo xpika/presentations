e637aa0a217deaaf056928c4e60be26ce4b92cba
Alan Hawkins <hawk.alan@gmail.com>
2014-11-28 13:47:31 +1100

    added misnomers

---
+++ fold_101/misnomers.hs
@@ -0,0 +1,10 @@
+
+
+eg1a = foldl1 (^) (reverse [2,3,4])
+eg1b = foldr1 (^) (reverse [2,3,4])
+
+eg1_1a = foldl1 (++) (reverse ["hello","world"])
+
+eg2a = take 10 $ foldr (:) [] (repeat 1)
+eg2b = take 10 $ foldl (flip (:)) [] (repeat 1)
+
