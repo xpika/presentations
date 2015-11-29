70df4c08dc9aa59b21a10777e87986d53ac1658b
Alan Hawkins <alanpika_au@yahoo.com>
2015-11-29 22:17:55 +1100

    Create code.hs

---
+++ blog/tagsoup/code.hs
@@ -0,0 +1,13 @@
+import Data.Char
+import Text.HTML.TagSoup
+import Text.HTML.TagSoup.Tree
+import System.IO
+
+main = do
+  -- after doing wget https://en.wikipedia.org/wiki/Main_Page
+  handle <- openFile "Main_Page.html" ReadMode
+  hSetEncoding handle utf8_bom
+  contents <- hGetContents handle
+  let html = renderTags . head . filter ((~== "<div id=mp-tfa>") . head) $ map flattenTree $ map (\x ->[x])  $  universeTree $ tagTree $ parseTags contents
+  hSetEncoding stdout utf8_bom
+  putStr html
