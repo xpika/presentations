93ed423c14c9a7557a295e48ec240298c8403ec4
Alan Hawkins <hawk.alan@gmail.com>
2015-12-14 20:48:39 +1100

    remove redundant divs.

--- blog/tagsoup/code.hs
+++ blog/tagsoup/code.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
 import Data.Char
 import Text.HTML.TagSoup
 import Text.HTML.TagSoup.Tree
@@ -14,3 +16,15 @@ main = do
   let html = renderTags . head . filter ((~== "<div id=mp-tfa>") . head) $ map flattenTree $ map (\x ->[x])  $  universeTree $ tagTree $ parseTags contents
   hSetEncoding stdout utf8_bom
   putStr html
+  
+main2 = do 
+        let a = renderTags $ flattenTree $ transformTree f $ tagTree  $ parseTags "<div><div>test</div></div>"
+        print a
+        return 32
+        
+    -- remove redundant divs
+f (TagBranch "div" atts [TagBranch "div" atts2 inner] ) = [TagBranch "div" atts inner]
+f x = [x]
+
+
+trans = undefined
